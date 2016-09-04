5eeb6b46ef3561aa299608c88ef55d6914b7d144
Bekka Murphy <bekkam@users.noreply.github.com>
2016-09-04 01:52:45 -0700

    Add feature to handle stock

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,8 @@ module ProductsHelper
     def print_price(price)
         number_to_currency price
     end
+
+    def print_stock(number)
+        number > 0 ? content_tag(:span, "In Stock #{number}", class: "in_stock") : content_tag(:span, "Out of Stock", class: "out_stock")
+    end
 end
